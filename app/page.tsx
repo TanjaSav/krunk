9b0417cc777a40039f4b9e213e10c89d90872c06
Olibjarki <olibjarkig@gmail.com>
2026-01-22 14:13:27 +0000

    made nav bar for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar";
 import Writepost from "./components/writepost";
 import TweetFeed from "./components/tweetfeed";
 import getTweets  from "./lib/getTweets";
+import Mobilenav from "./components/mobilenav";
 
 
 export default async function Home() {
@@ -12,6 +13,7 @@ export default async function Home() {
       <Writepost />
       <TweetFeed tweets={tweets} />
        <Navbar />
+       <Mobilenav />
     </main>
   );
 }
