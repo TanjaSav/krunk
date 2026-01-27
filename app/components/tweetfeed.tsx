ee35e65d3a301b237b1a6220b8b613df95f98f00
Eirikur1 <eirikurak@gmail.com>
2026-01-27 11:46:21 +0000

    Hide scrollbar in feed while keeping scroll functionality

--- app/components/tweetfeed.tsx
+++ app/components/tweetfeed.tsx
@@ -43,7 +43,7 @@ export default function TweetFeed({ tweets, username }: TweetFeedProps) {
       </div>
 
       {/* Scrollable posts list */}
-      <div className="flex-1 overflow-y-auto min-h-0">
+      <div className="flex-1 overflow-y-auto min-h-0 scrollbar-hide">
         {tweets.map((tweet: any) =>
           tweet.authorName === username ? (
             <Yourpost
