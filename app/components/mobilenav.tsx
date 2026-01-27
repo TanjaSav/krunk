5b5b0ffb6285daeb68c08b6b9eacd400d486283d
sahararos <sahararosin@gmail.com>
2026-01-27 13:48:18 +0000

    fixed mobile version

--- app/components/mobilenav.tsx
+++ app/components/mobilenav.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 
 export default function Mobilenav() {
-    return (
-        <>
-            {/* Header */}
-            <header className="fixed top-0 left-0 w-full h-22 bg-black border-b border-[#727272] flex justify-between items-center px-6 md:hidden">
-                <Image
-                    src="/images/logo.svg" alt="Logo" width={47} height={50}
-                    className="object-contain"
-                />
-                {/* Search input */}
-                <div className="relative">
+  return (
+    <>
+      {/* Header */}
+      <header className="fixed top-0 left-0 w-full h-22 bg-black border-b border-[#727272] flex justify-between items-center px-6 md:hidden">
+        <Image
+          src="/images/logo.svg"
+          alt="Logo"
+          width={47}
+          height={50}
+          className="object-contain"
+        />
+        <Image
+          src="/images/krunk-letter.svg"
+          alt="Logo"
+          width={80}
+          height={80}
+          className="object-contain"
+        />
+        {/* Search input */}
+        {/* <div className="relative">
                     <input
                         type="text" 
                         placeholder="Leita"
@@ -20,46 +30,51 @@ export default function Mobilenav() {
                         src="/images/search.svg" alt="Search icon" width={15} height={15} 
                         className="absolute left-2 top-1/2 transform -translate-y-1/2 color-[#8B99A6]"
                     />
-                </div>
-            </header>
+                </div> */}
+      </header>
 
-            {/* Footer */}
-            <footer className="fixed bottom-0 left-0 w-full h-22 bg-black border-t border-[#727272] flex justify-around items-center md:hidden">
-                <div className="flex items-center gap-4">
-                    <Image
-                        src="/images/circle.png" 
-                        alt="Profile Picture" width={42} height={42}
-                    />
-                    <div>
-                        <div className="text-poppins text-sm text-white font-regular">
-                        Name
-                        </div>
-                        <div className="text-poppins text-sm text-[#8B99A6] font-regular ">
-                        @username
-                        </div>
-                    </div>
-                </div>
+      {/* Footer */}
+      <footer className="fixed bottom-0 left-0 w-full h-22 bg-black border-t border-[#727272] flex justify-around items-center md:hidden">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/images/circle.png"
+            alt="Profile Picture"
+            width={42}
+            height={42}
+          />
+          <div>
+            <div className="text-poppins text-sm text-white font-regular">
+              Name
+            </div>
+            <div className="text-poppins text-sm text-[#8B99A6] font-regular ">
+              @username
+            </div>
+          </div>
+        </div>
 
-                {/*  Navigation links */}
-                <ul className="flex flex-row gap-16">
-                    <li>
-                    <Image src="/images/home.svg" 
-                        alt="Heim" width={24} height={24}
-                    />
-                    </li>
-                    <li className="">
-                    <Image
-                        src="/images/notifications.svg" 
-                        alt="Tilkynningar" width={24} height={24}
-                    />
-                    </li>
-                    <li className="">
-                    <Image src="/images/logout.svg" 
-                        alt="Skrá út" width={24} height={24} 
-                    />
-                    </li>
-                </ul>
-            </footer>
-        </>
-    );
+        {/*  Navigation links */}
+        <ul className="flex flex-row gap-16">
+          <li>
+            <Image src="/images/home.svg" alt="Heim" width={24} height={24} />
+          </li>
+          <li className="">
+            <Image
+              src="/images/notifications.svg"
+              alt="Tilkynningar"
+              width={24}
+              height={24}
+            />
+          </li>
+          <li className="">
+            <Image
+              src="/images/logout.svg"
+              alt="Skrá út"
+              width={24}
+              height={24}
+            />
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
 }
